c34ab34f24ae719027033c16506557a2610d8b97
alanborges69 <alan176966@gmail.com>
2017-03-15 20:30:34 -0300

    Add files via upload
    alteração apontando cliente para varias profissoes

---
+++ Cliente.java
@@ -0,0 +1,88 @@
+package br.edu.faculdadedelta.modelo;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="Cliente")
+public class Cliente extends BaseEntity<Long> {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id_cliente")
+	private Long id;
+	
+	@ManyToOne(cascade= {CascadeType.PERSIST}, fetch=FetchType.LAZY)
+	@JoinColumn(name="id_profissao", referencedColumnName="id_profissao",
+	   insertable=true, updatable=false, nullable=false)
+	private Profissao idProf;
+	
+	@Column(length=60, nullable=false)
+	private String nomeCliente;
+	@Column(length=20)
+	private String cpfCliente;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="dt_nascimento")
+	private Date dataNascimento;
+	
+	
+	public Cliente() {
+	}
+	
+	
+	public Cliente(Long id, Profissao idProf, String nomeCliente,
+			String cpfCliente, Date dataNascimento) {
+		this.id = id;
+		this.idProf = idProf;
+		this.nomeCliente = nomeCliente;
+		this.cpfCliente = cpfCliente;
+		this.dataNascimento = dataNascimento;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Profissao getIdProf() {
+		return idProf;
+	}
+	public void setIdProf(Profissao idProf) {
+		this.idProf = idProf;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public String getCpfCliente() {
+		return cpfCliente;
+	}
+	public void setCpfCliente(String cpfCliente) {
+		this.cpfCliente = cpfCliente;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+}
